8e569dc28b7dd93adeec8eebfaf0f5cba604abe0
ttwange <ttwange2@gmail.com>
2023-09-25 16:31:16 +0300

    working on the insert statement of the cassandra

--- flows/cass_consumer.py
+++ flows/cass_consumer.py
@@ -13,10 +13,8 @@ consumer = KafkaConsumer(
     auto_offset_reset='earliest',  # You can choose 'earliest' or 'latest' based on your needs
 )
 
-
 # Cassandra Configuration
 cassandra_host = 'localhost'  # Update with your Cassandra host address
-cassandra_keyspace = 'your_keyspace'  # Update with your Cassandra keyspace
 
 cluster = Cluster([cassandra_host])
 session = cluster.connect()
@@ -29,9 +27,13 @@ session.execute("""
 
 print("Keyspace created successfully!")
 
+cassandra_keyspace = 'crypto_data'  # Update with your Cassandra keyspace
+# Set the keyspace explicitly
+session.execute("USE crypto_data")
+
 # Define the CREATE TABLE statement
 create_table_statement = f"""
-    CREATE TABLE IF NOT EXISTS your_table (
+    CREATE TABLE IF NOT EXISTS Crypto_asset (
         id UUID PRIMARY KEY,
         rank INT,
         symbol TEXT,
@@ -51,7 +53,7 @@ session.execute(create_table_statement)
 
 # Create a prepared statement for inserting data into Cassandra
 insert_statement = session.prepare("""
-    INSERT INTO your_table (
+    INSERT INTO crypto_asset (
         id,
         rank,
         symbol,
@@ -66,49 +68,10 @@ insert_statement = session.prepare("""
     ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 """)
 
-
-# # Start consuming and writing data
 # Start consuming and writing data
-for message in consumer:
-    message_value = message.value.decode('utf-8')
-    
-    try:
-        # Parse the JSON data
-        data = json.loads(message_value)
-        
-        # Extract values from the JSON data
-        id_value = data.get('id', None)
-        rank_value = data.get('rank', None)
-        symbol_value = data.get('symbol', None)
-        name_value = data.get('name', None)
-        supply_value = data.get('supply', None)
-        maxSupply_value = data.get('maxSupply', None)
-        marketCapUsd_value = data.get('marketCapUsd', None)
-        volumeUsd24Hr_value = data.get('volumeUsd24Hr', None)
-        priceUsd_value = data.get('priceUsd', None)
-        changePercent24Hr_value = data.get('changePercent24Hr', None)
-        vwap24Hr_value = data.get('vwap24Hr', None)
-        
-        # Insert the data into Cassandra
-        session.execute(insert_statement, (
-            id_value,
-            rank_value,
-            symbol_value,
-            name_value,
-            supply_value,
-            maxSupply_value,
-            marketCapUsd_value,
-            volumeUsd24Hr_value,
-            priceUsd_value,
-            changePercent24Hr_value,
-            vwap24Hr_value
-        ))
-    
-    except json.JSONDecodeError as e:
-        print(f"Failed to decode JSON: {e}")
-    except Exception as e:
-        print(f"Error processing message: {e}")
+
+
 
 # # Close the connections
-# session.shutdown()
-# cluster.shutdown()
+session.shutdown()
+cluster.shutdown()
