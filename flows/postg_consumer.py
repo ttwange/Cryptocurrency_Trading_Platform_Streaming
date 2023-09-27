45733f6eae01bc6dd7165b724c847fbe266e7940
ttwange <ttwange2@gmail.com>
2023-09-27 06:36:06 +0300

    Working on adding the batch job for postgres

--- flows/postg_consumer.py
+++ flows/postg_consumer.py
@@ -3,14 +3,14 @@ from kafka import KafkaConsumer
 import psycopg2
 
 # Kafka Consumer Configuration
-kafka_bootstrap_servers = 'localhost:9092'  # Update with your Kafka broker address
+kafka_bootstrap_servers = 'localhost:9092'  
 kafka_topic = 'asset'
 
 consumer = KafkaConsumer(
     kafka_topic,
     bootstrap_servers=kafka_bootstrap_servers,
     group_id='asset_consumer_group',
-    auto_offset_reset='earliest',  # You can choose 'earliest' or 'latest' based on your needs
+    auto_offset_reset='earliest',  
 )
 
 # PostgreSQL Configuration
